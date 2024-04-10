229b4f55937b39cf95c00b842acda90c206f4bf0
Guilherme <gui.mochila@gmail.com>
2024-04-10 06:53:02 +0200

    feat(templates): Add templates and static files

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,10 +7,16 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+
+	// Serving static files
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
+	// Http Handlers
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
-	mux.HandleFunc("PSOT /snippet/create", snippetCreatePost)
+	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
 	log.Print("Server start on :4000")
 
